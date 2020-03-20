59166679f2d6431f54c36e3c4054d73225afd25b
TilekSydykov <tilek.sydykov@iaau.edu.kg>
2020-03-20 16:56:45 +0600

    retrofit and recyclerview

--- app/src/main/java/kg/koronastaff/staffapp/MainActivity.java
+++ app/src/main/java/kg/koronastaff/staffapp/MainActivity.java
@@ -36,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
@@ -49,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
